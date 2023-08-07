12cb0f6a6a151a8eb9b1456b529ec085c2626dc3
Bolli1618 <89577373+Bolli1618@users.noreply.github.com>
2023-08-07 17:05:27 +0200

    Update mod.rs

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,4 +1,5 @@
 use std::vec::Vec;
+use fltk::{app, enums::FrameType, frame::Frame, image::{JpegImage}, prelude::*, window::Window, group::{Flex, FlexType}};
 
 fn normalize_buffer(buffer: Vec<i32>) -> Vec<Vec<i32>> {
     let mut out_v: Vec<Vec<i32>> = Vec::new();
@@ -8,4 +9,28 @@ fn normalize_buffer(buffer: Vec<i32>) -> Vec<Vec<i32>> {
     }
 
     out_v
+}
+
+pub fn gui() {
+    let app = app::App::default();
+    let mut win = Window::new(100, 100, 800, 450, "Image Editor");
+
+    let mut flex = Flex::new(0, 0, 500, 450, None);
+    flex.set_type(FlexType::Column);
+
+    let mut frame = Frame::default().size_of_parent().center_of(&win);
+    frame.set_frame(FrameType::BorderBox);
+    let mut image = JpegImage::load("test.jpg").unwrap();
+    let mut preview = image.clone();
+    preview.scale(frame.height(), frame.width(), true, true);
+    frame.set_image(Some(preview));
+
+    flex.fixed(&mut frame, 500);
+    flex.end();
+
+    win.make_resizable(false);
+    win.end();
+    win.show();
+
+    app.run().unwrap();
 }
