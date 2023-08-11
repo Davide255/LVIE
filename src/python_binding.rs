43ebdb2295fd21fa8ca36c007886a37b570c8afe
Davide <80689057+Davide255@users.noreply.github.com>
2023-08-11 17:14:32 +0200

    Update python_binding.rs
    Adapted to pass references

--- src/python_binding.rs
+++ src/python_binding.rs
@@ -9,19 +9,19 @@ use lib::*;
 #[pyfunction]
 #[pyo3(name = "adjust_saturation")]
 fn py_adjust_saturation(buffer: Vec<Vec<f64>>, added_value: f32) -> Vec<Vec<f64>> {
-    adjust_saturation(buffer, added_value)
+    adjust_saturation(&buffer, added_value)
 }
 
 #[pyfunction]
 #[pyo3(name = "adjust_exposure")]
 fn py_adjust_exposure(buffer: Vec<Vec<f64>>, added_value: f32) -> Vec<Vec<f64>> {
-    adjust_exposure(buffer, added_value)
+    adjust_exposure(&buffer, added_value)
 }
 
 #[pyfunction]
 #[pyo3(name = "convert_to_grayscale")]
 fn py_convert_to_grayscale(buffer: Vec<Vec<f64>>) -> Vec<f64> {
-    convert_to_grayscale(buffer)
+    convert_to_grayscale(&buffer)
 }
 
 #[pyfunction]
@@ -30,19 +30,19 @@ fn py_combine_grayscale_with_colored(
     gray_scale_buffer: Vec<f64>,
     buffer: Vec<Vec<f64>>,
 ) -> Vec<Vec<f64>> {
-    combine_grayscale_with_colored(gray_scale_buffer, buffer)
+    combine_grayscale_with_colored(&gray_scale_buffer, &buffer)
 }
 
 #[pyfunction]
 #[pyo3(name = "adjust_contrast")]
 fn py_adjust_contrast(buffer: Vec<Vec<f64>>, added_value: f32) -> Vec<Vec<f64>> {
-    adjust_contrast(buffer, added_value)
+    adjust_contrast(&buffer, added_value)
 }
 
 #[pyfunction]
 #[pyo3(name = "find_edges_mask")]
 fn py_find_edges_mask(buffer: Vec<f64>, image_size: (i32, i32), sigma: f64, size: i32) -> Vec<f64> {
-    find_edges_mask(buffer, image_size, sigma, size)
+    find_edges_mask(&buffer, image_size, sigma, size)
 }
 
 #[pymodule]
