5bfcd9f7a076b6bfceae8ac8235c61917b2015c7
Davide <80689057+Davide255@users.noreply.github.com>
2023-08-20 22:01:19 +0200

    Update test.rs

--- src/test.rs
+++ src/test.rs
@@ -1,16 +1,43 @@
-mod lib;
-mod helpers;
-mod log_mask;
+use palette::{Srgb, Hsl};
+use std::vec::Vec;
 
-use lib::Buffer;
-use palette::{Srgb, Hsv, Hsl, FromColor};
+mod buffer_struct;
+
+use buffer_struct::Buffer;
+
+use std::time::Instant;
+
+use image::{self, GenericImageView, Pixel};
 
 fn main() {
-    let buff: Buffer<Hsl> = Buffer::<Hsl> {
-        buffer: vec![Hsl::from_color(Srgb::new(100.0 / 255.0, 100.0 /255.0, 100.0/255.0))]
-    };
 
-    let new_buff: Buffer<Hsv> = buff.convert_to_color::<Hsv>();
+    println!("Starting...");
 
-    println!("{:#?}",new_buff.convert_to_f64());
+    let now = Instant::now();
+
+    let img = image::open("src\\prova.jpg").unwrap();
+
+    println!("Image loaded!\n - Image size: {:?}", img.dimensions());
+
+    let mut buff: Vec<Vec<f64>> = Vec::new();
+
+    for i in img.pixels(){
+        let rgb: Vec<u8> = i.2.channels().to_vec();
+        buff.push(vec![(rgb[0] / 255) as f64, (rgb[1] / 255) as f64, (rgb[2] / 255) as f64]);
+    }
+
+    println!("Buffer created (len: {})", buff.len());
+
+    let mut buffer: Buffer = Buffer::<Srgb>::from_f64_buffer(&buff);
+
+    let elapsed = now.elapsed();
+
+    println!("Loaded buffer\n - Elapsed: {}", elapsed.as_secs());
+
+    let now = Instant::now();
+
+    let new_buffer: Buffer<Hsl> = buffer.convert_to::<Hsl>();
+
+    println!("Converted buffer to Hsl\n - Elapsed: {}", now.elapsed().as_secs())
+
 }
