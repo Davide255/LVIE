7369201fd4a0a7e806f416e06d4e436a80358659
Davide <80689057+Davide255@users.noreply.github.com>
2023-08-30 20:59:20 +0200

    Delete src directory

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::vec;
-
-use rust_backend::gui::gui;
-use fltk::{app, enums::FrameType, frame::Frame, image::{SvgImage, JpegImage}, prelude::*, window::Window};
-
-fn main() {
-    gui();
-}
