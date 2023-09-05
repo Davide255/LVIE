268a0ee78ecda8d87915bc62303c8ad1283f821d
Davide <80689057+Davide255@users.noreply.github.com>
2023-09-05 15:28:51 +0200

    Add files via upload

--- LVIE/build.rs
+++ LVIE/build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    slint_build::compile("ui/LVIE.slint").unwrap();
+    slint_build::compile("ui\\LVIE.slint").unwrap();
 }
--- LVIE/src/main.rs
+++ LVIE/src/main.rs
@@ -1,42 +1,67 @@
 #![allow(non_snake_case)]
 slint::include_modules!();
 
-use slint::{Image, SharedPixelBuffer, Rgb8Pixel};
-use image;
 use i_slint_backend_winit::WinitWindowAccessor;
+use image;
+use slint::{Image, Rgb8Pixel, SharedPixelBuffer, Weak};
 
-fn main() -> Result<(), slint::PlatformError> {
+#[allow(unused_imports)]
+use std::{thread, time};
+
+mod loading;
+
+#[allow(unreachable_code)]
+fn main() {
     slint::platform::set_platform(Box::new(i_slint_backend_winit::Backend::new()))
         .expect("Failed to set winit backend!");
 
-    let Window = HelloWorld::new()?;
+    let Window: LVIE = LVIE::new().unwrap();
 
-    let Window_weak = Window.as_weak();
-
-    let img = image::open("..\\..\\original.jpg")
-        .expect("Failed to open the image");
+    let img = image::open("original.jpg").expect("Failed to open the image");
 
     let pix_buf = SharedPixelBuffer::<Rgb8Pixel>::clone_from_slice(
-        img.as_rgb8().unwrap(), img.width(), img.height()
+        img.as_rgb8().unwrap(),
+        img.width(),
+        img.height(),
     );
 
     let slint_image = Image::from_rgb8(pix_buf);
 
     Window.set_image(slint_image);
 
-    Window.on_rust_init(move ||{
-        let ui = Window_weak.unwrap();
-        ui.window().with_winit_window(|winit_window: &winit::window::Window| {
-            winit_window.set_maximized(true);
-            winit_window.set_title("LVIE");
-        }).expect("Failed to use winit!");
-    });
+    Window
+        .global::<ToolbarCallbacks>()
+        .on_open_file_callback(|| {
+            println!("Called!");
+        });
 
-    //Window.window().with_winit_window(|winit_window: &winit::window::Window| {
-    //    winit_window.set_minimized(true);
-    //    winit_window.set_title("LVIE");
-    //}).expect("Failed to use winit!");
+    let maximize_ui = |ui: LVIE| {
+        ui.window()
+            .with_winit_window(|winit_window: &winit::window::Window| {
+                winit_window.set_maximized(true);
+                winit_window.set_title("LVIE");
+            })
+            .expect("Failed to use winit!");
+    };
 
+    let l_weak: Weak<LVIE> = Window.as_weak();
 
-    Window.run()
+    thread::Builder::new()
+        .name("waiter".to_string())
+        .spawn(move || {
+            thread::sleep(time::Duration::from_millis(100));
+            l_weak
+                .upgrade_in_event_loop(move |handle| {
+                    while !handle.window().is_visible() {
+                        thread::sleep(time::Duration::from_millis(10));
+                    }
+                    maximize_ui(handle);
+                })
+                .expect("Failed to call from the main thread");
+        })
+        .expect("Failed to spawn thread");
+
+    let _ = Window.show();
+    slint::run_event_loop().expect("Cannnot run the evnt loop due to an error!");
+    let _ = Window.hide();
 }
